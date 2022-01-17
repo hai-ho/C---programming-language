55ccdf0a6a04c3afca3ddc108858516dabac12f9
hai-ho <77719489+hai-ho@users.noreply.github.com>
2022-01-17 17:58:01 +0700

    Create EditForm.cs

---
+++ Tìm hiểu về giao thức UDP/Project6/EditForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project6
+{
+    public partial class EditForm : Form
+    {
+        public int Id { get { return (int)numericUpDown_Id.Value; } }
+        public string FullName { get { return textBox1.Text; } }
+        public int Year { get { return (int)numericUpDown_Year.Value; } }
+        public string Address { get { return textBox2.Text; } }
+
+        public EditForm(int Id, string name, int year, string address)
+        {
+            InitializeComponent();
+
+            numericUpDown_Id.Value = Id;
+            numericUpDown_Year.Value = year;
+            textBox1.Text = name;
+            textBox2.Text = address;
+        }
+    }
+}
