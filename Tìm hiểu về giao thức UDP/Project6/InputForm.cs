913fe07e55f050043df77f3cac1a0de21c2866a5
hai-ho <77719489+hai-ho@users.noreply.github.com>
2022-01-17 17:58:36 +0700

    Create InputForm.cs

---
+++ Tìm hiểu về giao thức UDP/Project6/InputForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project6
+{
+    public partial class InputForm : Form
+    {
+        public string FullName { get { return textBox1.Text; } }
+        public int Year { get { return (int)numericUpDown1.Value; } }
+        public string Address { get { return textBox2.Text; } }
+
+        public InputForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
